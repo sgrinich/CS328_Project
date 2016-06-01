673b494646c91df9c4233454a103c795ffb94271
sgrinich <sgrinich@gmail.com>
2016-05-31 19:06:09 -0500

    got graph and input working

--- comparison_plot.py
+++ comparison_plot.py
@@ -4,9 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt # import the plotting tools
 import collected_data_conversion
 
-
-person = "Rosa Parks"
-
 object_lst = ['William Shakespeare', 'Darth Vader',
 	'Madonna',
 	'Usain Bolt',
@@ -23,6 +20,15 @@ object_lst = ['William Shakespeare', 'Darth Vader',
 	'Jane Austen',
 	'Serena Williams']
 
+person = raw_input("Please enter a name from the following list. Make sure to spell it the same way.\n" +  str(object_lst))
+print
+while person not in object_lst:
+	person = raw_input("Please enter a name from the following list. Make sure to spell it the same way.\n" +  str(object_lst))
+	print
+
+
+
+
 object_lst_sans_person = []
 for character in object_lst:
 	if character != person:
@@ -34,14 +40,32 @@ tversky_results = model_results[0]
 shepard_results = model_results[1]
 medin_results = model_results[2]
 
+experiment_map_dict = {}
+ranking = len(experiment_results)
+for i in experiment_results:
+	experiment_map_dict[i] = ranking
+	ranking -= 1
 
+tversky_map_dict = {}
+ranking = len(tversky_results)
+for i in tversky_results:
+	tversky_map_dict[i] = ranking
+	ranking -= 1
 
+shepard_map_dict = {}
+ranking = len(shepard_results)
+for i in shepard_results:
+	shepard_map_dict[i] = ranking
+	ranking -= 1
 
 
 plt.ylabel('Ranking of Similarity to ' + person)
 plt.xlabel("People compared to " + person)
 plt.xticks(range(len(object_lst_sans_person)), object_lst_sans_person, size='small')
-plt.plot([x for x in range(16)], [x for x in range(0,16)])
+experiment, = plt.plot([x for x in range(15)], [experiment_map_dict[i] for i in object_lst_sans_person], label = "Experimental Results")
+tversky, = plt.plot([x for x in range(15)], [tversky_map_dict[i] for i in object_lst_sans_person], label = "Tversky Model Results")
+shepard, = plt.plot([x for x in range(15)], [shepard_map_dict[i] for i in object_lst_sans_person], label = "Shepard Model Results")
+plt.legend([experiment, tversky, shepard], ['Experimental Results', 'Tversky Model Results', "Shepard Model Results"])
 
 plt.title('Similarity to ' + person)
 plt.show()
